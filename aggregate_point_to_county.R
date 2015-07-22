5fbdf7aebced98d259e6513be6ed6539a13f2b95
Hao Hu <haohu3@illinois.edu>
2015-07-22 14:25:32 -0500

    update geojson

--- aggregate_point_to_county.R
+++ aggregate_point_to_county.R
@@ -5,7 +5,7 @@ pt = read.csv("illinois_case1.csv") # replace input data here
 pt.geo = pt
 
 # read illinois county geojson
-il = readOGR("il_counties.geojson", "OGRGeoJSON")
+il = readOGR("il.geojson", "OGRGeoJSON")
 coordinates(pt.geo) <- c("lon", "lat")
 proj4string(pt.geo) <- proj4string(il)
 
@@ -13,7 +13,7 @@ proj4string(pt.geo) <- proj4string(il)
 county.label <- over(pt.geo, il)
 
 # assign Name(county name) attribute from polygon to point
-pt$county <- county.label$Name
+pt$county <- county.label$County
 
 # aggregate by a combined key set
 out <- aggregate(yield ~ run + year + county, FUN = mean, data=pt, na.rm=T)
